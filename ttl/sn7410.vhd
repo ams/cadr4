542f9d97fd9a585cb5dd35d6e52809187cb5e242
Alfred M. Szmidt <ams@gnu.org>
2023-10-30 18:52:16 +0100

    ttl/sn7410.vhd: Add file.

---
+++ ttl/sn7410.vhd
@@ -0,0 +1,28 @@
+-- Tripple 3-Input Positive-NAND Gates
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity sn7410 is
+  port (
+    g1a, g1b, g1c : in  std_logic;
+    g1y_n         : out std_logic;
+
+    g2a, g2b, g2c : in  std_logic;
+    g2y_n         : out std_logic;
+
+    g3a, g3b, g3c : in  std_logic;
+    g3y_n         : out std_logic
+    );
+end sn7410;
+
+architecture ttl of sn7410 is
+begin
+
+  g1y_n <= not (g1a and g1b and g1c);
+  g2y_n <= not (g2a and g2b and g2c);
+  g3y_n <= not (g3a and g3b and g3c);
+
+end ttl;
+
