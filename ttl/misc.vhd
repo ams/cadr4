9e7b47b9f9393b09ac85fea2db71a1c1470f720b
Alfred M. Szmidt <ams@gnu.org>
2023-11-09 10:59:10 +0100

    ttl/misc.vhd (clkgen): New procedure.

--- ttl/misc.vhd
+++ ttl/misc.vhd
@@ -3,6 +3,8 @@ use ieee.std_logic_1164.all;
 
 package misc is
 
+  procedure clkgen(signal clk : out std_logic; constant frequency : real);
+
   function pullup(s   : std_logic) return std_logic;
   function pulldown(s : std_logic) return std_logic;
 
@@ -17,6 +19,22 @@ package misc is
 end;
 
 package body misc is
+
+  procedure clkgen(signal clk : out std_logic; constant frequency : real) is
+    constant period    : time := 1 sec / frequency;   -- Full period.
+    constant high_time : time := period / 2;          -- High time.
+    constant low_time  : time := period - high_time;  -- Low time; always >= HIGH_TIME.
+  begin
+    assert (high_time /= 0 fs)
+      report "clkgen: High time is zero; time resolution to large for frequency."
+      severity failure;
+    loop
+      clk <= '1';
+      wait for high_time;
+      clk <= '0';
+      wait for low_time;
+    end loop;
+  end procedure;
 
   function pullup(s : std_logic) return std_logic is
     variable pull : std_logic;
