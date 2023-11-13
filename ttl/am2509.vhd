f1b3816ad65889de9c563a3b835f312c9f8122d2
Alfred M. Szmidt <ams@gnu.org>
2023-11-13 17:50:30 +0100

    ttl/am2509.vhd: Implement something ..

--- ttl/am2509.vhd
+++ ttl/am2509.vhd
@@ -4,6 +4,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+library ttl;
+use ttl.misc.all;
+
 entity am2509 is
   port (
     clk    : in  std_logic;
@@ -20,6 +23,21 @@ entity am2509 is
 end am2509;
 
 architecture ttl of am2509 is
+
+  signal i0, i1, i2, i3 : std_logic;
+
 begin
+
+  i0 <= sel and (a0 or a1);
+  u0 : ff_d port map (clk => clk, d => i0, q => aq);
+
+  i1 <= sel and (b0 or b1);
+  u1 : ff_d port map (clk => clk, d => i1, q => bq);
+
+  i2 <= sel and (c0 or c1);
+  u2 : ff_d port map (clk => clk, d => i2, q => cq);
+
+  i3 <= sel and (d0 or d1);
+  u3 : ff_d port map (clk => clk, d => i3, q => dq);
 
 end ttl;
