7ed4d3afdd2dc0da868247436bb37a47341d30d4
Alfred M. Szmidt <ams@gnu.org>
2023-11-13 17:37:47 +0100

    ttl/ff_d.vhd: Implement.

--- ttl/ff_d.vhd
+++ ttl/ff_d.vhd
@@ -7,12 +7,19 @@ use ieee.numeric_std.all;
 entity ff_d is
   port (
     clk    : in  std_logic;
-    d   : in  std_logic;
+    d      : in  std_logic;
     q, q_n : out std_logic
     );
 end;
 
 architecture ttl of ff_d is
 begin
+
+  process (clk)
+  begin
+    if rising_edge(clk) then
+      q <= d;
+    end if;
+  end process;
 
 end;
