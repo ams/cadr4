ac9f7402393f5d41f25b5a1f2e5beb11468c3874
Alfred M. Szmidt <ams@gnu.org>
2023-11-19 23:11:44 +0100

    ttl/sn7411_tb.vhd, ttl/sn7414_tb.vhdp: Add tests.

--- ttl/sn7411_tb.vhd
+++ ttl/sn7411_tb.vhd
@@ -22,10 +22,35 @@ begin
     );
 
   process
-  begin
-    wait for 5 ns;
+    type pt is record
+      i0, i1, i2 : std_logic;
+      q          : std_logic;
+    end record;
+    type pa is array (natural range <>) of pt;
 
-    report "Testbench not implemented!" severity warning;
+    constant p : pa :=
+      (('0', '0', '0', '0'),
+       ('0', '0', '1', '0'),
+       ('0', '1', '0', '0'),
+       ('0', '1', '0', '0'),
+       ('1', '0', '0', '0'),
+       ('1', '0', '1', '0'),
+       ('1', '1', '0', '0'),
+       ('1', '1', '1', '1')
+       );
+
+  begin
+    for i in p'range loop
+      g1a <= p(i).i0; g1b <= p(i).i1; g1c <= p(i).i2;
+      g2a <= p(i).i0; g2b <= p(i).i1; g2c <= p(i).i2;
+      g3a <= p(i).i0; g3b <= p(i).i1; g3c <= p(i).i2;
+
+      wait for 1 ns;
+
+      assert g1y = p(i).q;
+      assert g2y = p(i).q;
+      assert g3y = p(i).q;
+    end loop;
 
     wait;
   end process;
--- ttl/sn7414_tb.vhd
+++ ttl/sn7414_tb.vhd
@@ -9,41 +9,54 @@ end;
 
 architecture testbench of sn7414_tb is
 
-  signal g1a   : std_logic;
-  signal g1q_n : std_logic;
-  signal g2a   : std_logic;
-  signal g2q_n : std_logic;
-  signal g3a   : std_logic;
-  signal g3q_n : std_logic;
-  signal g4a   : std_logic;
-  signal g4q_n : std_logic;
-  signal g5a   : std_logic;
-  signal g5q_n : std_logic;
-  signal g6a   : std_logic;
-  signal g6q_n : std_logic;
+  signal g1a, g1q_n : std_logic;
+  signal g2a, g2q_n : std_logic;
+  signal g3a, g3q_n : std_logic;
+  signal g4a, g4q_n : std_logic;
+  signal g5a, g5q_n : std_logic;
+  signal g6a, g6q_n : std_logic;
 
 begin
 
   uut : sn7414 port map(
-    g1a   => g1a,
-    g1q_n => g1q_n,
-    g2a   => g2a,
-    g2q_n => g2q_n,
-    g3a   => g3a,
-    g3q_n => g3q_n,
-    g4a   => g4a,
-    g4q_n => g4q_n,
-    g5a   => g5a,
-    g5q_n => g5q_n,
-    g6a   => g6a,
-    g6q_n => g6q_n
+    g1a => g1a, g1q_n => g1q_n,
+    g2a => g2a, g2q_n => g2q_n,
+    g3a => g3a, g3q_n => g3q_n,
+    g4a => g4a, g4q_n => g4q_n,
+    g5a => g5a, g5q_n => g5q_n,
+    g6a => g6a, g6q_n => g6q_n
     );
 
   process
-  begin
-    wait for 5 ns;
 
-    report "Testbench not implemented!" severity warning;
+    type pt is record
+      i0 : std_logic;
+      q  : std_logic;
+    end record;
+    type pa is array (natural range <>) of pt;
+
+    constant p : pa :=
+      (('0', '1'),
+       ('1', '0'));
+
+  begin
+    for i in p'range loop
+      g1a <= p(i).i0;
+      g2a <= p(i).i0;
+      g3a <= p(i).i0;
+      g4a <= p(i).i0;
+      g5a <= p(i).i0;
+      g6a <= p(i).i0;
+
+      wait for 1 ns;
+
+      assert g1q_n = p(i).q;
+      assert g2q_n = p(i).q;
+      assert g3q_n = p(i).q;
+      assert g4q_n = p(i).q;
+      assert g5q_n = p(i).q;
+      assert g6q_n = p(i).q;
+    end loop;
 
     wait;
   end process;
