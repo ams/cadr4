844cc2d2b07f2f30b55fea83d8c580d2cb58021b
Alex Segura <alex@lispm.dev>
2023-12-22 16:06:47 -0700

    Implement 74182 carry-lookahead

--- ttl/sn74182_tb.vhd
+++ ttl/sn74182_tb.vhd
@@ -48,10 +48,123 @@ begin
     );
 
   process
-  begin
-    wait for 5 ns;
+    type gpt is record
+      y3, y2, y1, y0 : std_logic;
+      x3, x2, x1 : std_logic;
+      y : std_logic;
+    end record;
+    type gpa is array (natural range <>) of gpt;
 
-    report "Testbench not implemented!" severity warning;
+    type ppt is record
+      x3, x2, x1, x0 : std_logic;
+      x : std_logic;
+    end record;
+    type ppa is array (natural range <>) of ppt;
+
+    type cxpt is record
+      y0, x0, cn : std_logic;
+      cx : std_logic;
+    end record;
+    type cxpa is array (natural range <>) of cxpt;
+
+    type cypt is record
+      y1, y0, x1, x0, cn : std_logic;
+      cy : std_logic;
+    end record;
+    type cypa is array (natural range <>) of cypt;
+
+    type czpt is record
+      y2, y1, y0, x2, x1, x0, cn : std_logic;
+      cz : std_logic;
+    end record;
+    type czpa is array (natural range <>) of czpt;
+
+    constant g : gpa :=
+      (('0', '1', '1', '0', '1', '1', '0', '0'),
+       ('1', '0', '1', '0', '0', '1', '0', '0'),
+       ('1', '0', '0', '1', '0', '0', '0', '0'),
+       ('1', '0', '1', '0', '0', '0', '0', '0'),
+       ('1', '1', '1', '0', '1', '0', '0', '1'),
+       ('1', '1', '1', '0', '1', '0', '0', '1'),
+       ('1', '1', '1', '0', '1', '1', '0', '1'),
+       ('1', '0', '1', '0', '1', '1', '1', '1'));
+
+    constant p : ppa :=
+      (('0', '0', '0', '0', '0'),
+       ('0', '1', '0', '0', '1'),
+       ('0', '0', '1', '0', '1'),
+       ('1', '0', '0', '0', '1'),
+       ('0', '1', '0', '0', '1'),
+       ('1', '1', '0', '0', '1'),
+       ('1', '1', '0', '1', '1'),
+       ('1', '1', '1', '1', '1'));
+
+    constant cx : cxpa :=
+      (('0', '0', '0', '1'),
+       ('0', '0', '1', '1'),
+       ('0', '1', '0', '1'),
+       ('0', '1', '1', '1'),
+       ('1', '0', '1', '1'),
+       ('1', '0', '0', '0'),
+       ('1', '0', '1', '1'),
+       ('1', '1', '1', '0'));
+
+    constant cy : cypa :=
+      (('0', '1', '1', '1', '1', '1'),
+       ('0', '0', '1', '1', '1', '1'),
+       ('0', '1', '0', '1', '1', '1'),
+       ('0', '1', '0', '1', '0', '1'),
+       ('0', '1', '0', '1', '1', '1'),
+       ('0', '0', '0', '1', '1', '1'),
+       ('0', '1', '0', '0', '1', '1'),
+       ('1', '1', '0', '0', '0', '0'));
+
+    constant cz : czpa :=
+      (('0', '0', '0', '0', '0', '0', '0', '1'),
+       ('1', '0', '1', '0', '0', '0', '0', '1'),
+       ('1', '1', '0', '0', '0', '1', '0', '1'),
+       ('1', '1', '1', '0', '0', '0', '1', '1'),
+       ('1', '1', '1', '1', '0', '0', '1', '0'),
+       ('1', '1', '1', '1', '1', '0', '1', '0'),
+       ('1', '1', '1', '1', '1', '1', '1', '0'));
+
+  begin
+    for i in g'range loop
+      y3 <= g(i).y3; y2 <= g(i).y2; y1 <= g(i).y1; y0 <= g(i).y0;
+      x3 <= g(i).x3; x2 <= g(i).x2; x1 <= g(i).x1;
+      wait for 3 ns;
+      assert yout = g(i).y report "failure in case: " & integer'image(i);
+    end loop;
+
+    for i in p'range loop
+      x3 <= p(i).x3; x2 <= p(i).x2; x1 <= p(i).x1; x0 <= p(i).x0;
+      wait for 3 ns;
+      assert xout = p(i).x report "failure in case: " & integer'image(i);
+    end loop;
+
+    for i in cx'range loop
+      y0 <= cx(i).y0;
+      x0 <= cx(i).x0;
+      cin_n <= cx(i).cn;
+      wait for 3 ns;
+      assert cout0_n = cx(i).cx report "failure in case: " & integer'image(i);
+    end loop;
+
+    for i in cy'range loop
+      y1 <= cy(i).y1; y0 <= cy(i).y0;
+      x1 <= cy(i).x1; x0 <= cy(i).x0;
+      cin_n <= cy(i).cn;
+      wait for 3 ns;
+      assert cout1_n = cy(i).cy report "failure in case: " & integer'image(i);
+    end loop;
+
+    for i in cz'range loop
+      y2 <= cz(i).y2; y1 <= cz(i).y1; y0 <= cz(i).y0;
+      x2 <= cz(i).x2; x1 <= cz(i).x1; x0 <= cz(i).x0;
+      cin_n <= cz(i).cn;
+      wait for 3 ns;
+      assert cout2_n = cz(i).cz report "failure in case: " & integer'image(i);
+    end loop;
 
     wait;
   end process;
