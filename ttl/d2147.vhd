c5dc7a367e01f526dd5f33818a59c9d0db0eacf1
Mete Balci <metebalci@gmail.com>
2025-06-24 09:38:07 +0200

    Add d2147 component and testbench, include PDF documentation
    - Introduced d2147 VHDL component for 4096 x 1 bit static RAM.
    - Added testbench for d2147 to verify functionality.
    - Included PDF documentation for d2147 component.

--- ttl/d2147.vhd
+++ ttl/d2147.vhd
@@ -1,11 +1,12 @@
--- 4096 X 1 Static RAM
+-- 4096 X 1 Bt Static RAM
+-- Intel 2147H
+-- doc/ttl/d2147.pdf
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- Datasheet: AMD AM2147
-entity am2147 is
+entity d2147 is
   port (
     a0   : in  std_logic := 'H';
     a1   : in  std_logic := 'H';
@@ -24,21 +25,27 @@ entity am2147 is
     we_n : in  std_logic := 'H';
     do   : out std_logic
     );
-end am2147;
+end entity;
 
-architecture ttl of am2147 is
+architecture ttl of d2147 is
   type ram_t is array (0 to 4095) of std_logic;
   signal ram : ram_t;
 begin
   process (all)
     variable addr : unsigned(11 downto 0);
   begin
-    addr := (a11, a10, a9, a8, a7, a6, a5, a4, a3, a2, a1, a0);
     if ce_n = '0' then
-      if we_n = '0' then
-        ram(to_integer(addr)) <= di;
+      addr := (a11, a10, a9, a8, a7, a6, a5, a4, a3, a2, a1, a0);
+      if is_x(addr) then
+        if we_n = '1' then
+          do <= 'X';
+        end if;
       else
-        do <= ram(to_integer(addr));
+        if we_n = '0' then
+          ram(to_integer(addr)) <= di;
+        else
+          do <= ram(to_integer(addr));
+        end if;
       end if;
     else
       do <= 'Z';
---
+++ ttl/intel.vhd
@@ -0,0 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+package intel is
+
+  component d2147
+    port(
+      a0   : in  std_logic;
+      a1   : in  std_logic;
+      a2   : in  std_logic;
+      a3   : in  std_logic;
+      a4   : in  std_logic;
+      a5   : in  std_logic;
+      do   : out std_logic;
+      we_n : in  std_logic;
+      ce_n : in  std_logic;
+      di   : in  std_logic;
+      a11  : in  std_logic;
+      a10  : in  std_logic;
+      a9   : in  std_logic;
+      a8   : in  std_logic;
+      a7   : in  std_logic;
+      a6   : in  std_logic);
+  end component;
+
+end;
