7709b71d80a7c8de266db551c54d1d71ace551ff
Mete Balci <metebalci@gmail.com>
2025-06-26 08:42:43 +0200

    Initialize TTLDM outputs to high for proper clock operation; add comments for clarity on signal behavior and initialization requirements.

--- ttl/ttldm.vhd
+++ ttl/ttldm.vhd
@@ -5,17 +5,31 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+-- This is a critical component for the clock to tick properly.
+-- Proper clock after power on reset is:
+-- -TPR40 being a 40ns low pulse with 220ns period.
+-- TPCLK having 220ns period with 160ns high pulse, 60ns low pulse.
+
+-- The outputs of TTLDM should be initialized to H or 1 for this to work.
+-- If not H or 1, S'R' latches do not work properly in clock1 and clock2 
+-- because -tpr/-tpw signals are used as S' or R' and H or 1 is a not care
+-- value. If they are 0, it is an illegal state.
+
+-- The input is TTL, pulled weak high.
+-- Maybe one interpretation is because they are pulled high, the outputs are 
+-- also expected to be H with no inputs.
+
 entity ttldm is
   generic (
     single_tap_delay : time := 5 ns
     );
   port (
     input  : in  std_logic;
-    tap1   : out std_logic := '0';
-    tap2   : out std_logic := '0';
-    tap3   : out std_logic := '0';
-    tap4   : out std_logic := '0';
-    output : out std_logic := '0'
+    tap1   : out std_logic := 'H';
+    tap2   : out std_logic := 'H';
+    tap3   : out std_logic := 'H';
+    tap4   : out std_logic := 'H';
+    output : out std_logic := 'H'
     );
 end ttldm;
 
@@ -23,8 +37,8 @@ architecture behavioral of ttldm is
   signal input_int : std_logic;
   signal input_var : std_logic;
 begin
-  input_int <= 'H'; input_int <= input;
-  
+  input_int <= 'H';
+  input_int <= input;
   input_var <= to_x01(input_int);
   tap1 <= transport input_var after single_tap_delay;
   tap2 <= transport input_var after single_tap_delay * 2;
